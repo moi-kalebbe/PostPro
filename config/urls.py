701e5008429b3c152ddf92aec677bc96de9f0e0d
Moisés Kalebbe <kalebbe.moises@gmail.com>
2026-01-10 18:01:30 -0300

    fix(config): enable media serving in production

--- config/urls.py
+++ config/urls.py
@@ -3,9 +3,10 @@ URL configuration for PostPro.
 """
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     # Django Admin
@@ -37,3 +38,10 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # Serve media files in production (only if not using Nginx/S3)
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    ]
